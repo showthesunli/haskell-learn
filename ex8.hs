fb7fbbdb3340ccc70bc89ada774aee821181eac7
showthesunli <showthesunli@gmail.com>
2022-11-19 23:57:37 +0800

    accomplished ex8.hs

--- ex8.hs
+++ ex8.hs
@@ -6,3 +6,19 @@ myRe = foldr (\x xs -> xs ++ [x]) []
 
 ys :: [Int]
 ys = [1, 3, 4]
+
+mf :: (a -> b) -> (a -> Bool) -> [a] -> [b]
+mf f p xs = map f (filter p xs)
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f xs = foldr (\x xs -> (f x) : xs) [] xs
+
+myD :: Int -> [a] -> [a]
+myD n (x : xs)
+  | n == 0 = x : xs
+  | n > 0 = myD (n - 1) xs
+
+reLa :: [a] -> [a]
+reLa (x : xs)
+  | null xs = []
+  | otherwise = x : reLa xs
